6cded53c3ea0f08712837631345ddae5b14e9d8c
Aryansabasana <aryan.sabasana.cd@gmail.com>
2026-05-27 20:19:09 +0530

    setup backend project structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const homeRoutes = require("./routes/home.routes");
-const problemRoutes = require("./routes/problem.routes");
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
-app.use("/", homeRoutes);
-app.use("/",problemRoutes);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+
+app.get((req,res) => {
+    res.send("Go Epic Backend Running");
+})
+
+
 
 module.exports = app;
